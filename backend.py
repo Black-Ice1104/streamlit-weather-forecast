0f3973271197ed142cdf67bab5ce676980ec8a6c
Bellamy <931588213@qq.com>
2023-02-16 00:03:38 -0800

    2.16 Commit: Complete email content.

---
+++ backend.py
@@ -0,0 +1,36 @@
+
+import requests
+
+API_KEY = "33945f61ed45603d2cb17ef86700b517"
+API_KEY_2 = "75732110e5199414f9d57155fbed54a6"
+units = ["metric", "imperial", ""] # temperature in Celsius / Fahrenheit / Kelvin (absolute temperature scale)
+def get_data(place, forecast_days=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units={units[0]}"
+    # Los Angeles: https://api.openweathermap.org/data/2.5/forecast?q=los%20angeles&appid=75732110e5199414f9d57155fbed54a6&units=metric
+
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+
+    # Adjust time according to time zone
+    time_zone = data["city"]["timezone"] / 3600  # e.g., Los Angeles: -28800 (GMT-9), Shanghai: 28800 (GMT+9)
+    time_shift = round(time_zone / 3)  # the forecast gets updated every 3 hours
+    if time_shift < 0:  # if later than GMT, move temperature line to former
+        for x in range(0, 40 + time_shift):
+            filtered_data[x]["main"]["temp"] = filtered_data[x - time_shift]["main"]["temp"]
+    elif time_shift > 0:  # if earlier than GMT, move temperature line to later, and set earlier temperature to null
+        for x in reversed(range(39 - time_shift)):
+            filtered_data[x + time_shift]["main"]["temp"] = filtered_data[x]["main"]["temp"]
+        # same thing:
+        # for x in range(39 - time_shift, -1, -1):
+        # or
+        # for x in range(0, 40 - time_shift):
+        #     filtered_data[39 - x]["main"]["temp"] = filtered_data[39 - x - time_shift]["main"]["temp"]
+        for x in range(0, time_shift):
+            filtered_data[x]["main"]["temp"] = None
+
+    # Forecast update 8 times a day
+    update_times = 8 * forecast_days
+    filtered_data = filtered_data[:update_times]
+
+    return filtered_data
---
+++ frontend.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import streamlit as st
+import plotly.express as px
+from backend import get_data
+
+def web():
+    # Add title, text input, slider, select box and subheader
+    st.title("Weather Forecast for the Next Days")
+    place = st.text_input("Place: ")
+    days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecast days")
+    option = st.selectbox("Select data to view",
+                          ("Temperature", "Sky"))
+    # using fstring because the header is dynamic (any change in place, days or option would change the header)
+    st.subheader(f"{option} for the next {days} days in {place}")
+
+    if place:
+        try:
+            # Get the temperature / sky data
+            filtered_data = get_data(place, days)
+
+            if option == "Temperature":
+                temperatures = [dict["main"]["temp"] for dict in filtered_data]
+                dates = [dict["dt_txt"] for dict in filtered_data]
+                # Create a temperature plot
+                figure = px.line(x=dates, y=temperatures, labels={"x": "Dates", "y": "Temperature (C)"})
+                st.plotly_chart(figure, use_container_width=True)
+
+            if option == "Sky":
+                images = {"Clear": "images/clear.png",
+                          "Clouds": "images/cloud.png",
+                          "Rain": "images/rain.png",
+                          "Snow": "images/snow.png"}
+                sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+                image_paths = [images[condition] for condition in sky_conditions]
+                text_layout = [condition for condition in sky_conditions]
+                st.image(image_paths, width=132)
+                # st.caption(text_layout)
+        except KeyError:
+            st.write("The place does not exist. Please modify the place name and press Enter again.")
