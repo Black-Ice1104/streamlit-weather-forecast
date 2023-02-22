ed7a75aaa87e0d89acd1339b7fc08dbe61da8f91
Bellamy <931588213@qq.com>
2023-02-21 23:57:28 -0800

    2.16 Commit: add configurations files including Procfile and setup.sh for heroku

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[email_key]\n\
+sender_addr = \"michaelice2604@gmail.com\"\n\
+sender_pswd = \"hkkjsmtnckbcxcwp\"\n\
+[weather_key]\n\
+API_KEY = \"33945f61ed45603d2cb17ef86700b517\"\n\
+" > ~/.streamlit/credentials.toml
