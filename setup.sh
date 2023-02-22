f37e5ffa65b79194de876e044070d5af9beb9ec4
Bellamy <931588213@qq.com>
2023-02-22 01:00:04 -0800

    2.16 Commit: add config.toml to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ sender_pswd = \"hkkjsmtnckbcxcwp\"\n\
 [weather_key]\n\
 API_KEY = \"33945f61ed45603d2cb17ef86700b517\"\n\
 " > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
