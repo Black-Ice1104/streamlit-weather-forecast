ec887ad8dd80c3739fbb7ad5a6814778482f4139
Bellamy <931588213@qq.com>
2023-03-10 13:31:38 -0800

    3.10 1st Commit: adapt for streamlit web app 5

--- pages/Subscribe.py
+++ pages/Subscribe.py
@@ -48,56 +48,56 @@ if cache == Null:
     else:  # login, but not save cookies
         value = get_address()
         cache = value
-        if IS_RELEASED == "True":
-            cookies = cache
-        else:
+        if IS_RELEASED == "False":
             cookies['a-cookie'] = cache
+        else:
+            cookies = cache
         cookies.save()  # Force saving the cookies now, without a rerun
         st.experimental_rerun()
 else:  # login, with cookies saved
-    st.write("cookies != Null")
-    # address = cache
-    # # Subscribe
-    # with st.form(key="subscribe_forms"):
-    #     st.subheader("Logged in as " + address)
-    #     city = st.text_input("The city / region to subscribe")
-    #     firstname = st.text_input("Your firstname")
-    #     lastname = st.text_input("Your lastname")
-    #     button = st.form_submit_button("Subscribe Now")
-    #     if button:
-    #         try:
-    #             repeat = store_subscribe(firstname, lastname, address, city)
-    #             if not repeat:
-    #                 send_subscribe(firstname, address, city)
-    #                 st.info("Subscription Successful!")
-    #             else:
-    #                 st.info("Sorry, you have already subscribed!")
-    #         except KeyError:
-    #             st.info("The place does not exist. Please modify the place name and subscribe again.")
-    # if st.button("Log out"):  # log out, delete cookies, open a new tab
-    #     cache = Null
-    #     if IS_RELEASED == "True":
-    #         cookies = cache
-    #     else:
-    #         cookies['a-cookie'] = cache
-    #     cookies.save()
-    #     st.info("You have logged out. Please close the window.", icon="ℹ️")
-    #     time.sleep(1)
-    #     webbrowser.open(main, new=0)
-    #     st.stop()
-    # cookies.save()  # Force saving the cookies now, without a rerun
-    #
-    # cities = cities_subscribed(address)
-    # if cities:
-    #     with st.form(key="unsubscribe_forms"):
-    #         st.subheader("Unsubscribe")
-    #         city_to_del = st.multiselect("Select city / region to unsubscribe", cities)
-    #         submit = st.form_submit_button(label="Submit")
-    #         if submit:
-    #             for city_name in city_to_del:
-    #                 unsubscribe(address, city_name)
-    #                 st.write("Successfully unsubscribed ", city_name)
-    # else:
-    #     st.info("You have not subscribed any cities yet!")
+    # st.write("cookies != Null")
+    address = cache
+    # Subscribe
+    with st.form(key="subscribe_forms"):
+        st.subheader("Logged in as " + address)
+        city = st.text_input("The city / region to subscribe")
+        firstname = st.text_input("Your firstname")
+        lastname = st.text_input("Your lastname")
+        button = st.form_submit_button("Subscribe Now")
+        if button:
+            try:
+                repeat = store_subscribe(firstname, lastname, address, city)
+                if not repeat:
+                    send_subscribe(firstname, address, city)
+                    st.info("Subscription Successful!")
+                else:
+                    st.info("Sorry, you have already subscribed!")
+            except KeyError:
+                st.info("The place does not exist. Please modify the place name and subscribe again.")
+    if st.button("Log out"):  # log out, delete cookies, open a new tab
+        cache = Null
+        if IS_RELEASED == "False":
+            cookies['a-cookie'] = cache
+        else:
+            cookies = cache
+        cookies.save()
+        st.info("You have logged out. Please close the window.", icon="ℹ️")
+        time.sleep(1)
+        webbrowser.open(main, new=0)
+        st.stop()
+    cookies.save()  # Force saving the cookies now, without a rerun
+
+    cities = cities_subscribed(address)
+    if cities:
+        with st.form(key="unsubscribe_forms"):
+            st.subheader("Unsubscribe")
+            city_to_del = st.multiselect("Select city / region to unsubscribe", cities)
+            submit = st.form_submit_button(label="Submit")
+            if submit:
+                for city_name in city_to_del:
+                    unsubscribe(address, city_name)
+                    st.write("Successfully unsubscribed ", city_name)
+    else:
+        st.info("You have not subscribed any cities yet!")
 
 
