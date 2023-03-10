9666c6be8099feffa9ae290ff7331bb37151ba7a
Bellamy <931588213@qq.com>
2023-03-09 21:19:44 -0800

    3.9 5th Commit: check cookies on cloud

--- pages/Subscribe.py
+++ pages/Subscribe.py
@@ -27,38 +27,38 @@ cookies = EncryptedCookieManager(
 
 if not cookies.ready():
     st.stop()
-
-if cookies['a-cookie'] == '':
-    if url == original:  # not login
-        st.write(get_login_str(), unsafe_allow_html=True)  # show "Login with Google"
-    else:  # login, but not save cookies
-        value = get_address()
-        cookies['a-cookie'] = value
-        cookies.save()  # Force saving the cookies now, without a rerun
-        st.experimental_rerun()
-else:  # login, with cookies saved
-    address = cookies['a-cookie']
-    with st.form(key="email_forms"):
-        st.subheader("Logged in as " + address)
-        city = st.text_input("The city / region to subscribe")
-        firstname = st.text_input("Your firstname")
-        lastname = st.text_input("Your lastname")
-        button = st.form_submit_button("Subscribe Now")
-        if button:
-            try:
-                repeat = store_subscribe(firstname, lastname, address, city)
-                if not repeat:
-                    send_subscribe(firstname, address, city)
-                    st.info("Subscription Successful!")
-                else:
-                    st.info("Sorry, you have already subscribed!")
-            except KeyError:
-                st.info("The place does not exist. Please modify the place name and subscribe again.")
-    if st.button("Log out"):  # log out, delete cookies, open a new tab
-        cookies['a-cookie'] = ''
-        cookies.save()
-        st.info("You have logged out. Please close the window.", icon="ℹ️")
-        time.sleep(1)
-        webbrowser.open(main, new=0)
-        st.stop()
-    cookies.save()  # Force saving the cookies now, without a rerun
+st.write(cookies)
+# if cookies['a-cookie'] == '':
+#     if url == original:  # not login
+#         st.write(get_login_str(), unsafe_allow_html=True)  # show "Login with Google"
+#     else:  # login, but not save cookies
+#         value = get_address()
+#         cookies['a-cookie'] = value
+#         cookies.save()  # Force saving the cookies now, without a rerun
+#         st.experimental_rerun()
+# else:  # login, with cookies saved
+#     address = cookies['a-cookie']
+#     with st.form(key="email_forms"):
+#         st.subheader("Logged in as " + address)
+#         city = st.text_input("The city / region to subscribe")
+#         firstname = st.text_input("Your firstname")
+#         lastname = st.text_input("Your lastname")
+#         button = st.form_submit_button("Subscribe Now")
+#         if button:
+#             try:
+#                 repeat = store_subscribe(firstname, lastname, address, city)
+#                 if not repeat:
+#                     send_subscribe(firstname, address, city)
+#                     st.info("Subscription Successful!")
+#                 else:
+#                     st.info("Sorry, you have already subscribed!")
+#             except KeyError:
+#                 st.info("The place does not exist. Please modify the place name and subscribe again.")
+#     if st.button("Log out"):  # log out, delete cookies, open a new tab
+#         cookies['a-cookie'] = ''
+#         cookies.save()
+#         st.info("You have logged out. Please close the window.", icon="ℹ️")
+#         time.sleep(1)
+#         webbrowser.open(main, new=0)
+#         st.stop()
+#     cookies.save()  # Force saving the cookies now, without a rerun
